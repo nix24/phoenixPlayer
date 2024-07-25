c04a277edbc86649db3cbc4ee0f694fed20c6a29
Jaylon Carrington <carringtonjaylon4@gmail.com>
2024-07-25 10:40:01 -0400

    removing gitignore

---
+++ src/lib/wasmPkg/wasm_utils_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function add(a: number, b: number): number;
+export function format_bytes(a: number, b: number, c: number): void;
+export function format_time(a: number, b: number): void;
+export function search_songs(a: number, b: number, c: number, d: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
